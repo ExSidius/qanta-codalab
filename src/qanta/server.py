154c1c74347212292ef0e148f0e7af43a3aa3eea
ExSidius <mukul.ram97@gmail.com>
2019-05-10 18:33:50 -0400

    Initial setup

---
+++ src/qanta/server.py
@@ -0,0 +1,24 @@
+
+EMBEDDINGS_PATH = ''
+
+
+class Guesser:
+    def __init__(self):
+        pass
+
+    @property
+    def vectorizer(self):
+        with open(EMBEDDINGS_PATH, 'r') as f:
+            pass
+
+    def train(self):
+        pass
+
+    def guess(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
